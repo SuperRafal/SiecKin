1a4f0ab4ac33cd7afb141c8000b785e42a4e1e92
wiktorb93 <wikpr@o2.pl>
2015-05-11 11:03:46 +0200

    Cennik

---
+++ Service/Cennik/CennikFacade.cs
@@ -0,0 +1,55 @@
+﻿using SiecKin.Model;
+using SiecKin.Service.Cennik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiecKin.Service.Cennik
+{
+    public class CennikFacade
+    {
+        public CennikFacade()
+        {
+            service = new CennikService();
+        }
+
+        private CennikService service;
+
+        public List<TypBiletu> WyswietlCennik()
+        {
+            return service.WyswietlCennik();
+        }
+
+        public void ZmienParametry(TypBiletu b, decimal cena, string rodzaj, Nullable<System.DataTime> obowiazuje_do)
+        {
+            service.ZmienParametry(b, cena, rodzaj, obowiazuje_do)
+        }
+
+        public void ZmienCene(TypBiletu b, decimal cena)
+        {
+            service.ZmienCene(b, cena);
+        }
+
+        public void ZmienRodzaj(TypBiletu b, string rodzaj)
+        {
+            service.ZmienRodzaj(b, rodzaj);
+        }
+
+        public void ZmienDate(TypBiletu b, Nullable<System.DataTime> obowiazuje_do)
+        {
+            service.ZmienDate(b, obowiazuje_do);
+        }
+      
+
+        public void UsunBilet(TypBiletu b)
+        {
+            service.UsunBilet(b);
+        }
+
+        public void DodajBilet(TypBiletu b)
+        {
+            service.DodajBilet(b);
+        }
+    }
+}
---
+++ Service/Cennik/CennikService.cs
@@ -0,0 +1,64 @@
+﻿using SiecKin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiecKin.Service.Cennik
+{
+    public class CennikService
+    {
+        public CennikService()
+        {
+            dc = new SiecKinEntities();
+        }
+
+        private SiecKinEntities dc;
+
+        public List<TypBiletu> WyswietlCennik()
+        {
+            var query = from b in dc.Cennik
+                        select b;
+            return query.ToList();
+        }
+
+        public void ZmienParametry(TypBiletu b, decimal cena, string rodzaj, Nullable<System.DataTime> obowiazuje_do)
+        {
+            b.cena = cena;
+            b.rodzaj = rodzaj;
+            b.obowiazuje_od = DateTime.Now;
+            b.obowiazuje_do = obowiazuje_do;
+        }
+
+        public void ZmienCene(TypBiletu b, decimal cena)
+        {
+            b.cena = cena;
+            b.obowiazuje_od = DateTime.Now;
+        }
+
+        public void ZmienRodzaj(TypBiletu b, string rodzaj)
+        {
+            b.rodzaj = rodzaj;
+            b.obowiazuje_od = DateTime.Now;
+        }
+
+        public void ZmienDate(TypBiletu b, Nullable<System.DataTime> obowiazuje_do)
+        {
+            b.obowiazuje_do = obowiazuje_do;
+            b.obowiazuje_od = DateTime.Now;
+        }
+      
+
+        public void UsunBilet(TypBiletu b)
+        {
+            dc.Cennik.Remove(b);
+            dc.SaveChanges();
+        }
+
+        public void DodajBilet(TypBiletu b)
+        {
+            dc.Cennik.Add(b);
+            dc.SaveChanges();
+        }
+    }
+}
