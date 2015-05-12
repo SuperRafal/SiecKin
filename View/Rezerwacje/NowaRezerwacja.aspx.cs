27b9b729e91de19ec2b78ac448043010b1f43475
Mateusz Brud <mbrud94@gmail.com>
2015-05-12 18:36:02 +0200

    ViewPart3

--- View/Rezerwacje/NowaRezerwacja.aspx.cs
+++ View/Rezerwacje/NowaRezerwacja.aspx.cs
@@ -16,7 +16,6 @@ namespace SiecKin.View.Rezerwacje
         protected void Page_Load(object sender, EventArgs e)
         {
             facade = new RezerwacjeFacade();
-            r = facade.NowaRezerwacja();
             
             ListaMiast = facade.ZwrocMiasta();
             if (Page.IsPostBack == false)
@@ -25,20 +24,35 @@ namespace SiecKin.View.Rezerwacje
                 this.DropDownListMiasta.DataBind();
                 this.DropDownListKina.DataSource = facade.AdresyKinWMiescie(this.DropDownListMiasta.SelectedItem.Text);
                 this.DropDownListKina.DataBind();
-            }
 
+                data = DateTime.Now;
+                Session["data_seansu"] = data;
+                //int id = facade.ZwrocKino(this.DropDownListMiasta.SelectedItem.Text, this.DropDownListKina.SelectedItem.Text);
+                // this.ListaSeansow = facade.SeanseKinoDzien(id, data);
+                this.PokazSeanse();
+
+            }
+            else
+            {
+                try
+                {
+                    data = (DateTime)Session["data_seansu"];
+                }
+                catch { }
+            }
+            
             int i = 0;
             foreach (Button b in this.PlaceHolderDaty.Controls)
             {
                 DateTime d = DateTime.Now;
                 d = d.AddDays(i);
-                //b.Text = d.ToShortDateString();
                 b.Text = d.ToShortDateString() + Environment.NewLine + d.DayOfWeek.ToString();
                 i++;
-                //
-                b.Click += new EventHandler(test_click);
             }
-            
+
+     
+           this.ButtonDalej.Enabled = false;
+           this.Label4.Text = index_seansu.ToString();
             
         }
 
@@ -46,45 +60,43 @@ namespace SiecKin.View.Rezerwacje
         private Rezerwacja r;
         private List<string> ListaMiast;
         private List<Seans> ListaSeansow;
+        private int index_seansu;
+        private DateTime data;
 
         protected void DropDownListMiasta_SelectedIndexChanged(object sender, EventArgs e)
         {
             this.DropDownListKina.DataSource = facade.AdresyKinWMiescie(this.DropDownListMiasta.SelectedItem.Text);
             this.DropDownListKina.DataBind();
+            this.PokazSeanse();
 
         }
 
         protected void DataClick(object sender, EventArgs e)
         {
             Button b = (Button)sender;
-            //string[] s = b.Text.Split(' ');
             int i = this.PlaceHolderDaty.Controls.IndexOf(b);
-            DateTime d = DateTime.Now;
-            d = d.AddDays(i);
-            //this.Label4.Text = d.ToShortDateString();
-            int id = facade.ZwrocKino(this.DropDownListMiasta.SelectedItem.Text, this.DropDownListKina.SelectedItem.Text);
-            //this.Label4.Text += "<br />" + id.ToString();
-            this.ListaSeansow = facade.SeanseKinoDzien(id, d);
-            //this.Label4.Text += "<br />" + this.ListaSeansow.Count.ToString();
+            data = DateTime.Now;
+            data = data.AddDays(i);
+            //int id = facade.ZwrocKino(this.DropDownListMiasta.SelectedItem.Text, this.DropDownListKina.SelectedItem.Text);
+            //this.ListaSeansow = facade.SeanseKinoDzien(id, d);
             this.PokazSeanse();
+            this.Label4.Text = data.ToShortDateString();
+            Session["data_seansu"] = data;
         }
-        protected void test_click(object sender, EventArgs e)
-        {
-            Button b = (Button)sender;
-            int i = this.PlaceHolderDaty.Controls.IndexOf(b);
-            //this.Label4.Text += "<br/> kliknales button nr " + i.ToString();
-        }
-
-
-
 
         protected void ButtonDalej_Click(object sender, EventArgs e)
         {
+           // r = facade.NowaRezerwacja();
+            //this.Label4.Text = index_seansu.ToString();
+            //facade.UstawSeans(r, ListaSeansow.ElementAt(index_seansu));
+            //Session["rezerwacja"] = r;
             Server.Transfer("~/View/Rezerwacje/WyborMiejsca.aspx");
         }
         
         private void PokazSeanse()
         {
+            int id = facade.ZwrocKino(this.DropDownListMiasta.SelectedItem.Text, this.DropDownListKina.SelectedItem.Text);
+            this.ListaSeansow = facade.SeanseKinoDzien(id, data);
             DataTable dt = new DataTable();
             dt.Columns.Add("Film");
             dt.Columns.Add("Godzina");
@@ -98,18 +110,49 @@ namespace SiecKin.View.Rezerwacje
             this.GridView1.DataSource = dt;
            
             this.GridView1.DataBind();
-            foreach (GridViewRow r in this.GridView1.Rows)
-            {
-                CheckBox c = (CheckBox)r.FindControl("CheckBox1");
-                
-            }
+
 
             
         }
 
         protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
         {
-            this.Label4.Text = "poszlo";
+            CheckBox c = (CheckBox)sender;
+            if (c.Checked == false)
+                c.Checked = true;
+            index_seansu = -1;
+            this.ButtonDalej.Enabled = true;
+            foreach (GridViewRow row in this.GridView1.Rows)
+            {
+                CheckBox ce = (CheckBox)row.FindControl("CheckBox1");
+                if (ce != c)
+
+                    ce.Checked = false;
+                else
+                {
+                    index_seansu = row.RowIndex;
+                }
+                    
+                
+
+            }
+
+            r = facade.NowaRezerwacja();
+            int id = facade.ZwrocKino(this.DropDownListMiasta.SelectedItem.Text, this.DropDownListKina.SelectedItem.Text);
+            this.ListaSeansow = facade.SeanseKinoDzien(id, data);
+            facade.UstawSeans(r, ListaSeansow.ElementAt(index_seansu));
+            Session["rezerwacja"] = r;
+            //this.Label4.Text = index_seansu.ToString();
+        }
+
+        protected void ButtonTest_Click(object sender, EventArgs e)
+        {
+            this.Label4.Text +="<br />"+ index_seansu.ToString();
+        }
+
+        protected void DropDownListKina_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.PokazSeanse();
         }
       
 
--- View/Rezerwacje/WyborMiejsca.aspx
+++ View/Rezerwacje/WyborMiejsca.aspx
@@ -2,5 +2,18 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p><asp:Label ID="Label3" runat="server" Text="Wybierz miejsce"></asp:Label></p>
     <asp:PlaceHolder ID="PlaceHolderSala" runat="server"></asp:PlaceHolder>
+    <br />
+    <p>
+        <asp:Button ID="Button1" runat="server" Text="" Height="25px" Width="25px" />
+        <asp:Label ID="Label4" runat="server" Text="Zajęte miejsce"></asp:Label><br />
+        <asp:Button ID="Button2" runat="server" Text="" Height="25px" Width="25px"/>
+        <asp:Label ID="Label5" runat="server" Text="Wolne miejsce"></asp:Label><br />
+        <asp:Button ID="Button3" runat="server" Text="" Height="25px" Width="25px" />
+        <asp:Label ID="Label6" runat="server" Text="Wybrane miejsce"></asp:Label><br />
+    </p>
+    <asp:Button ID="ButtonDalej" runat="server" Text="Dalej" /><br />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label><br />
+    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
 </asp:Content>
