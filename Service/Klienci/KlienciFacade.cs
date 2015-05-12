65e76dbd73e5d18dae1af2ed9dd3b5c559becb38
donex93 <donex93@gmail.com>
2015-05-12 16:14:51 +0200

    Pracownicy/Klienci

---
+++ Service/Klienci/KlienciFacade.cs
@@ -0,0 +1,43 @@
+﻿using SiecKin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiecKin.Service.Klienci
+{
+    public partial class KlienciFacade
+    {
+        private KlienciService service;
+        public KlienciFacade()
+        {
+            service = new KlienciService();
+        }
+
+        public void DodajKlienta(Klient k)
+        {
+            service.DodajKlienta(k);
+        }
+        public void UsunKlienta(Klient k)
+        {
+            service.UsunKlienta(k);
+        }
+        public void ObliczID(Klient k)
+        {
+            service.ObliczID(k);
+        }
+        public Klient ZwrocKlienta(int id)
+        {
+            return service.ZwrocKlienta(id);
+        }
+        public List<int> ZwrocKlientow()
+        {
+            return service.ZwrocKlientow();
+        }
+        public void EdytujDane(Klient k, string e_mail, string haslo, string imie, string nazwisko, string login, string nr_konta, string nr_telefonu)
+        {
+            service.EdytujDane(k, e_mail, haslo, imie, nazwisko, login, nr_konta, nr_telefonu);
+        }
+
+    }
+}
---
+++ Service/Pracownicy/PracownicyFacade.cs
@@ -0,0 +1,42 @@
+﻿using SiecKin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiecKin.Service.Pracownicy
+{
+    public partial class PracownicyFacade
+    {
+        private PracownicyService service;
+
+        public PracownicyFacade()
+        {
+            service = new PracownicyService();
+        }
+        public void DodajPracownika(Pracownik p)
+        {
+            service.DodajPracownika(p);
+        }
+        public void UsunPracownika(Pracownik p)
+        {
+            service.UsunPracownika(p);
+        }
+        public void ObliczID(Pracownik p)
+        {
+            service.ObliczID(p);
+        }
+        public Pracownik ZwrocPracownika(int id)
+        {
+            return service.ZwrocPracownika(id);
+        }
+        public List<int> ZwrocPracownikow()
+        {
+            return service.ZwrocPracownikow();
+        }
+        public void EdytujDane(Pracownik p, string haslo, string imie, string nazwisko, string login)
+        {
+            service.EdytujDane(p, haslo, imie, nazwisko, login);
+        }
+    }
+}
